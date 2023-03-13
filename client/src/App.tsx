ea03cf29197f46ad40883bafb4a2247d79ff0dfa
noisyboy25 <dj402and@gmail.com>
2023-03-13 21:24:28 +0300

    Change env var name

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
   const [products, setProducts] = useState<Product[]>([]);
 
   const handleUpdateProducts = async () => {
-    const res = await fetch(`${import.meta.env.API_HOST}/api/products/`);
+    const res = await fetch(`${import.meta.env.VITE_API_HOST}/api/products/`);
     const data = await res.json();
     setProducts(data.products);
   };
